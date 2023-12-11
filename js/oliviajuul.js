d4fb0a379a38c97e64f1e34a770da26b36e4c142
Teresa <tehsv0001@stud.kea.dk>
2023-12-11 15:19:50 +0100

    footer + kontakt + plakater

--- js/oliviajuul.js
+++ js/oliviajuul.js
@@ -1,18 +1,28 @@
-let currentIndex = 0;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function changeSlide(n) {
-    currentIndex += n;
-    showSlides();
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showSlides() {
-    const slides = document.querySelector('.carousel');
-    if (currentIndex < 0) {
-        currentIndex = slides.children.length - 1;
-    } else if (currentIndex >= slides.children.length) {
-        currentIndex = 0;
-    }
-    slides.style.transform = `translateX(${-currentIndex * 100}%)`;
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-    showSlides();
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
